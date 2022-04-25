5960557586cf5cdc0b5f9afb56b4becbef8d6e99
choul0 <98428229+choul0@users.noreply.github.com>
2022-04-25 19:31:11 -0400

    Add files via upload

--- FrontEndDemo/DatatableDisplay.py
+++ FrontEndDemo/DatatableDisplay.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+import pymysql
 
 
-def UserInterface():
+def UserInterface(username, cnx):
     window = tk.Tk()
+
+    cur = cnx.cursor()
 
     canvas = tk.Canvas(window, height=600, width=800, bg="#8D91C7")
     canvas.pack()
@@ -17,9 +20,55 @@ def UserInterface():
     watchedListLabel.place(relx=0.4, rely=0.1)
 
     toWatchList = tk.Listbox(frame, fg="#2E5266", bg="#CFFCFF", font="Cambria, 12")
-    toWatchList.place(relx=0.1, rely=0.2,relheight=0.7,relwidth=0.25)
+    toWatchList.place(relx=0.1, rely=0.2, relheight=0.7, relwidth=0.25)
 
     watchedList = tk.Listbox(frame, fg="#2E5266", bg="#CFFCFF", font="Cambria, 12")
     watchedList.place(relx=0.4, rely=0.2, relheight=0.7, relwidth=0.25)
 
+    browseDatabase = tk.Button(frame, text="Browse Entire Database", font="Cambria, 12", command=databaseBrowser)
+    browseDatabase.place(relx=0.683, rely=0.833)
 
+    query = "SELECT email, dob, status FROM user_profile WHERE user_profile.username=\'" + username + "\'"
+    cur.execute(query)
+    list = cur.fetchall()
+
+    emailLabel = tk.Label(frame, text="Email:", bg="#B0DAF1", fg="#2E5266", font="Cambria, 14")
+    emailLabel.place(relx=0.78, rely=0.2)
+
+    emailLabel2 = tk.Label(frame, text=list[0]['email'], bg="#B0DAF1", fg="#2E5266", font="Cambria, 10")
+    emailLabel2.place(relx=0.72, rely=0.3)
+
+    dobLabel = tk.Label(frame, text="Date of Birth:\n" + str(list[0]['dob']), bg="#B0DAF1", fg="#2E5266",
+                        font="Cambria, 14")
+    dobLabel.place(relx=0.75, rely=0.4)
+
+    statusLabel = tk.Label(frame, text="Status: " + str(list[0]['status']), bg="#B0DAF1", fg="#2E5266",
+                           font="Cambria, 14")
+    statusLabel.place(relx=0.7, rely=0.6)
+
+    statusEntry = tk.Entry(frame)
+    statusEntry.place(relx=0.7, rely=0.65, relwidth=0.25)
+
+    updateStatus = tk.Button(frame, text="Update Status", command=lambda: statusUpdater(statusEntry.get(),
+                                                                                        username, cur, frame))
+    updateStatus.place(relx=0.7, rely=0.7)
+
+    window.mainloop()
+
+def statusUpdater(status, username, cur, frame):
+    query = "UPDATE user_profile SET status = \'"+status+"\' WHERE username=\'"+username+"\'"
+    cur.execute(query)
+
+    query = "SELECT email, dob, status FROM user_profile WHERE user_profile.username=\'" + username + "\'"
+    cur.execute(query)
+    list = cur.fetchall()
+
+    statusLabel = tk.Label(frame, text="Status: " + str(list[0]['status']), bg="#B0DAF1", fg="#2E5266",
+                           font="Cambria, 14")
+    statusLabel.place(relx=0.7, rely=0.6)
+
+    statusEntry = tk.Entry(frame)
+    statusEntry.place(relx=0.7, rely=0.65, relwidth=0.25)
+
+def databaseBrowser():
+    print("hi")
--- FrontEndDemo/main.py
+++ FrontEndDemo/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkcalendar import DateEntry
 import pymysql
 
 import DatatableDisplay
@@ -7,23 +8,132 @@ import DatatableDisplay
 def Login():
     username = usernameEntry.get()
     password = pwEntry.get()
+    userExist = False
+    if username=='root' and password=='root':
+        userExist = True
     if (username == "" and password == ""):
         displayErrorMessage("username and password cannot be empty")
         return
+    cur = cnx.cursor()
+    try:
+        cur.execute("SELECT username, password  FROM user_profile ")
+    except pymysql.Error as e:
+        print('something went wrong with the select statement')
+        exit()
+    list = cur.fetchall()
+    for row in list:
+        if row['username'] == username and row['password'] == password:
+            userExist = True
+            break;
+    if userExist:
+        msg = tk.Label(frame, text="Successfully logged in", bg="#B0DAF1", fg="#8CB369", font="Cambria, 10")
+        msg.place(relx=0.25, rely=0.75)
+        DatatableDisplay.UserInterface(username, cnx)
     else:
-        DatatableDisplay.UserInterface()
-    #check for username and password match
+        displayErrorMessage("Incorrect username or password")
 
 
 def Register():
-    username = usernameEntry.get()
-    password = pwEntry.get()
-    displayErrorMessage("something random for testing purposes")
-    #check if username exists
+    regWindow = tk.Tk()
+
+    canvas = tk.Canvas(regWindow, height=600, width=800, bg="#8D91C7")
+    canvas.pack()
+
+    frame = tk.Frame(regWindow, bg="#B0DAF1")
+    frame.place(relx=0.25, rely=0.3, relwidth=0.5, relheight=0.5)
+
+    label = tk.Label(frame, text="Register", bg="#B0DAF1", fg="#2E5266", font="Cambria, 25")
+    label.pack()
+
+    usernameLabel = tk.Label(frame, text="Username:", bg="#B0DAF1", fg="#2E5266", font="Cambria, 10")
+    usernameLabel.place(relx=0.15, rely=0.2)
+
+    usernameEntry = tk.Entry(frame)
+    usernameEntry.place(relx=0.32, rely=0.2, relwidth=0.5)
+
+    pwLabel = tk.Label(frame, text="Password:", bg="#B0DAF1", fg="#2E5266", font="Cambria, 10")
+    pwLabel.place(relx=0.155, rely=0.3)
+
+    pwEntry = tk.Entry(frame)
+    pwEntry.place(relx=0.32, rely=0.3, relwidth=0.5)
+
+    repwLabel = tk.Label(frame, text="Re-enter Password:", bg="#B0DAF1", fg="#2E5266", font="Cambria, 10")
+    repwLabel.place(relx=0.02, rely=0.4)
+
+    repwEntry = tk.Entry(frame)
+    repwEntry.place(relx=0.32, rely=0.4, relwidth=0.5)
+
+    emailLabel = tk.Label(frame, text="Email:", bg="#B0DAF1", fg="#2E5266", font="Cambria, 10")
+    emailLabel.place(relx=0.21, rely=0.5)
+
+    emailEntry = tk.Entry(frame)
+    emailEntry.place(relx=0.32, rely=0.5, relwidth=0.5)
+
+    dobLabel = tk.Label(frame, text="Date of Birth:", bg="#B0DAF1", fg="#2E5266", font="Cambria, 10")
+    dobLabel.place(relx=0.11, rely=0.6)
+
+    dobEntry = DateEntry(frame, date_pattern="yyyy-mm-dd")
+    dobEntry.place(relx=0.32, rely=0.6, relwidth=0.5)
+
+    registerButton = tk.Button(frame, text="register",
+                               command=lambda: validateRegInfo(usernameEntry.get(), pwEntry.get(),
+                                                               repwEntry.get(), emailEntry.get(),
+                                                               dobEntry.get(), frame))
+    registerButton.place(relx=0.45, rely=0.7, relwidth=0.2)
+
+    regWindow.mainloop()
+
+
+def validateRegInfo(username, pw1, pw2, email, dob, frame):
+    if username == "" or pw1 == "" or pw2 == "" or email == "" or dob == "":
+        msg = tk.Label(frame, text="The fields cannot be empty", bg="#B0DAF1", fg="#FF0000", font="Cambria, 10")
+        msg.place(relx=0.3, rely=0.8)
+        return;
+    if not pw1 == pw2:
+        msg = tk.Label(frame, text="The passwords are not matching", bg="#B0DAF1", fg="#FF0000", font="Cambria, 10")
+        msg.place(relx=0.3, rely=0.8)
+        return
+    if "@" not in email:
+        msg = tk.Label(frame, text="Invalid email format, must contain @", bg="#B0DAF1", fg="#FF0000",
+                       font="Cambria, 10")
+        msg.place(relx=0.3, rely=0.8)
+        return
+    cur = cnx.cursor()
+    try:
+        cur.execute("SELECT username, email  FROM user_profile ")
+    except pymysql.Error as e:
+        print('something went wrong with the select statement')
+        exit()
+    list = cur.fetchall()
+    for row in list:
+        if username == row['username']:
+            msg = tk.Label(frame, text="User already exists", bg="#B0DAF1", fg="#FF0000", font="Cambria, 10")
+            msg.place(relx=0.3, rely=0.8)
+            return
+        if email == row['email']:
+            msg = tk.Label(frame, text="This email is already taken", bg="#B0DAF1", fg="#FF0000", font="Cambria, 10")
+            msg.place(relx=0.3, rely=0.8)
+            return
+    query = "INSERT INTO user_profile VALUES (\"" + username + "\", \"" + pw1 + "\", \"" + email + "\", \'" + dob + "\', NULL)"
+    cur.execute(query)
+    msg = tk.Label(frame, text="User successfully added", bg="#B0DAF1", fg="#8CB369", font="Cambria, 10")
+    msg.place(relx=0.3, rely=0.8)
+
 
 def displayErrorMessage(text):
-    msg = tk.Label(frame, text=text,bg="#B0DAF1", fg="#FF0000", font="Cambria, 10")
+    msg = tk.Label(frame, text=text, bg="#B0DAF1", fg="#FF0000", font="Cambria, 10")
     msg.place(relx=0.25, rely=0.75)
+
+
+try:
+    # Change accordingly so that it's connected to the local host database
+    cnx = pymysql.connect(host='localhost', user="root",
+                          password="Louis_17380u470tp",
+                          db='anime', charset='utf8mb4',
+                          cursorclass=pymysql.cursors.DictCursor)
+except pymysql.Error as e:
+    print('Error: %d: %s' % (e.args[0], e.args[1]))
+    exit()
 
 window = tk.Tk()
 
